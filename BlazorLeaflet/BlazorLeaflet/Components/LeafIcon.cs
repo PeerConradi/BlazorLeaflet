f358d77490bdc230d7cac9a1add3d2548360576b
Aeuke Peer Conradi <peerconradi@gmail.com>
2021-06-26 14:46:15 +0200

    Refactored Markers with Components

---
+++ BlazorLeaflet/BlazorLeaflet/Components/LeafIcon.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace BlazorLeaflet.Components
+{
+    public class LeafIcon : ComponentBase, IDisposable
+    {
+        [CascadingParameter]public LeafMarkerBase ParentMarker { get; set; }
+
+        /// <summary>
+        /// (required) The URL to the icon image (absolute or relative to your script path).
+        /// </summary>
+        [Parameter] public string Url { get; set; }
+
+        /// <summary>
+        /// The URL to a retina sized version of the icon image (absolute or relative to your script path). Used for Retina screen devices.
+        /// </summary>
+        [Parameter] public string RetinalUrl { get; set; }
+
+        /// <summary>
+        /// Size of the icon image in pixels.
+        /// </summary>
+        [Parameter] public Size? Size { get; set; }
+
+        /// <summary>
+        /// The coordinates of the "tip" of the icon (relative to its top left corner). The icon will be aligned so that this point is at the marker's geographical location. Centered by default if size is specified, also can be set in CSS with negative margins.
+        /// </summary>
+        [Parameter] public Point? Anchor { get; set; }
+
+        /// <summary>
+        /// The coordinates of the point from which popups will "open", relative to the icon anchor.
+        /// </summary>
+        [Parameter] public Point PopupAnchor { get; set; } = Point.Empty;
+
+        /// <summary>
+        /// The coordinates of the point from which tooltips will "open", relative to the icon anchor.
+        /// </summary>
+        [Parameter] public Point TooltipAnchor { get; set; } = Point.Empty;
+
+        /// <summary>
+        /// The URL to the icon shadow image. If not specified, no shadow image will be created.
+        /// </summary>
+        [Parameter] public string ShadowUrl { get; set; }
+
+        [Parameter] public string ShadowRetinalUrl { get; set; }
+
+        /// <summary>
+        /// Size of the shadow image in pixels.
+        /// </summary>
+        [Parameter] public Size? ShadowSize { get; set; }
+
+        /// <summary>
+        /// The coordinates of the "tip" of the shadow (relative to its top left corner) (the same as iconAnchor if not specified).
+        /// </summary>
+        [Parameter] public Size? ShadowAnchor { get; set; }
+
+        /// <summary>
+        /// A custom class name to assign to both icon and shadow images. Empty by default.
+        /// </summary>
+        [Parameter] public string ClassName { get; set; } = string.Empty;
+
+        private Models.Icon icon;
+
+        protected override void OnParametersSet()
+        {
+            Console.WriteLine("OnParametersSet");
+            this.icon = new Models.Icon()
+            {
+                Anchor = this.Anchor,
+                ClassName = this.ClassName,
+                PopupAnchor = this.PopupAnchor,
+                RetinalUrl = this.RetinalUrl,
+                ShadowAnchor = this.ShadowAnchor,
+                ShadowRetinalUrl = this.ShadowRetinalUrl,
+                ShadowSize = this.ShadowSize,
+                ShadowUrl = this.ShadowUrl,
+                Size = this.Size,
+                TooltipAnchor = this.TooltipAnchor,
+                Url = this.Url
+            };
+            ParentMarker?.SetIcon(icon);
+        }
+
+        public void Dispose()
+        {
+            if (ParentMarker != null)
+            {
+                ParentMarker?.SetIcon(null);
+            }
+        }
+    }
+}
---
+++ BlazorLeaflet/BlazorLeaflet/Components/LeafMarkerBase.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorLeaflet.Components
+{
+    public class LeafMarkerBase : ComponentBase, IDisposable
+    {
+
+        [Parameter]public float Latitude { get; set; }
+
+        [Parameter]public float Longitude { get; set; }
+
+        private Models.Icon icon;
+
+        [CascadingParameter]public LeafletMap ParentMap { get; set; }
+
+        [Parameter]public RenderFragment ChildContent { get; set; }
+
+        Models.Marker _marker;
+
+        public LeafMarkerBase() : base()
+        {
+            if (this.ChildContent != null)
+            {
+                Console.WriteLine("Child content");
+            }
+            else
+            {
+                Console.WriteLine("No child content");
+            }
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (ParentMap.Map != null)
+            {
+                ParentMap.Map.OnInitialized += Map_OnInitialized;
+            }
+        }
+
+        private void Map_OnInitialized()
+        {
+            _marker = new Models.Marker(new Models.LatLng(Latitude, Longitude));
+            _marker.Icon = icon;
+            ParentMap.Map.AddLayer(_marker);
+            Console.WriteLine("Map_OnInitialized");
+        }
+
+        public void SetIcon(Models.Icon setIcon)
+        {
+            Console.WriteLine("SetIcon");
+            this.icon = setIcon;
+            if (_marker != null)
+            {
+                ParentMap.Map.RemoveLayer(_marker);
+                _marker.Icon = setIcon;
+                ParentMap.Map.AddLayer(_marker);
+                InvokeAsync(StateHasChanged);
+                
+            }
+            
+        }
+
+        public void Dispose()
+        {
+            if (_marker != null)
+                ParentMap?.Map?.RemoveLayer(_marker);
+        }
+    }
+}
